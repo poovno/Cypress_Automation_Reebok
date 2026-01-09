70e776cc0f061cd85710466f12ce3465c11ae4d9
Poovarasan S <poovno@gmail.com>
2026-01-09 15:59:38 +0530

    Stable homepage and login UI flow tests (OTP typing excluded

---
+++ cypress/e2e/homePage/login.cy.js
@@ -0,0 +1,49 @@
+describe("Login flow (Mobile number )- Reebok",()=>
+{
+
+    beforeEach(()=>{
+        cy.viewport(1440,900);
+        cy.visit("/");
+    
+    });
+
+    it("Login page opens Successfully",()=>
+    {
+      
+        cy.contains("Log in").click({force:true});
+        cy.get("body").should("be.visible");
+    });
+
+    it("Mobile number input field is visible",()=>{
+         cy.contains("Log in").click({force:true});
+         cy.get('input[type="mobile"],input[type="text"]').should("be.visible");
+
+    });
+    it("Shows invalidation error while entering mobile number",()=>{
+
+        cy.contains("Log in").click({force:true});
+
+        cy.get('input[type="tel"], input[type="text"]', { timeout: 10000 })
+  .filter(':visible')
+  .should('have.length',1)
+  .scrollIntoView()
+  .type("12345",{force:true});
+
+        cy.contains("GET OTP").filter(":visible").should("be.enabled")
+        .click({force:true});
+        cy.contains("*Please enter a valid 10 digit mobile number").should("exist");
+
+    });
+
+    it("Valid mobile number Navigates to OTP Screen",()=>
+    {
+        cy.contains("Log in").click({force:true});
+        cy.get('input[type="mobile"], input[type="text"]', { timeout: 10000 })
+  .filter(':visible')
+  .should("exist");
+  
+
+        cy.contains("GET OTP").filter(":visible").should("be.enabled")
+        
+    })
+});
