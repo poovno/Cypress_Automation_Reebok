3e01162897e304e16ad4c52a2f916849b0fe7695
Poovarasan.S <poovno@gmail.com>
2026-01-11 19:52:07 +0530

    Adding address function

---
+++ cypress/e2e/homePage/AddingDeliverAddress.cy.js
@@ -0,0 +1,38 @@
+// ✅ Helper function (define OUTSIDE describe)
+
+
+describe("Entering a valid delivery address", () => {
+
+
+
+  beforeEach(() => {
+    cy.viewport(1440, 900);
+    cy.visit("/");
+   
+    
+      cy.get('img[alt="close icon"]')
+        .should('be.visible')
+        .click({ force: true });
+  
+  
+});
+  it("Click and type delivery address", () => {
+
+    // Step 1: Open delivery location modal
+    cy.contains("Add delivery location")
+      .should("be.visible")
+      .click({ force: true });
+
+    // Step 2: Type pincode / location
+    cy.get('#addressInput', { timeout: 10000 })
+      .should('be.visible')
+      .clear()
+      .type("560059");
+
+    // Optional: validate address applied
+    cy.contains("Bengaluru").should("be.visible").click();
+   cy.contains("save location").should("be.visible").click().click().click();
+  });
+
+
+});
